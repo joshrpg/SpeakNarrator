70135df1183358676ba7288e8859c8b8e4785f2e
joshrpg <xjoshrpgx@gmail.com>
2019-11-15 15:49:20 -0600

    init creation of repo and files
    This is the inital creation of the repo. This has the 1.0.0 version release.

---
+++ src/xyz/joshrpg/SpeakNarrator/Main.java
@@ -0,0 +1,51 @@
+package xyz.joshrpg.SpeakNarrator;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import net.md_5.bungee.api.ChatMessageType;
+import net.md_5.bungee.api.chat.BaseComponent;
+import net.md_5.bungee.api.chat.TranslatableComponent;
+
+public class Main extends JavaPlugin {
+	
+	@Override public void onEnable() {
+		getServer().getPluginManager().registerEvents(new SpeakNarratorChatListener(), this);
+	}
+	
+	@Override public void onDisable() {}
+	
+	@Override public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (command.getName().equalsIgnoreCase("version")) {
+            sender.sendMessage("You are running version 1.0.0 of SpeakNarrator by Joshrpg");
+            return true;
+        }
+        return false;
+    }
+	
+	
+	public class SpeakNarratorChatListener implements Listener {
+		@EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = false)
+		public void onAsyncPlayerChatEvent(AsyncPlayerChatEvent event) {
+			if (event.isCancelled())
+				return;
+			
+			BaseComponent message;
+			
+			message = new TranslatableComponent("chat.type.text", event.getPlayer().getDisplayName(), event.getMessage());
+			
+			for (Player p : event.getRecipients()) {
+				p.spigot().sendMessage(ChatMessageType.CHAT, message);
+			}
+			
+			event.getRecipients().clear();
+		}
+	}
+	
+}
