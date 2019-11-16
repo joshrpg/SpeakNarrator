8c929035e8d3ba1e4d5d4dfe089e72717942113d
joshrpg <xjoshrpgx@gmail.com>
2019-11-15 20:16:12 -0600

    Updated team handeling for team colors

--- changelog.md
+++ changelog.md
@@ -1,4 +1,15 @@
 # Change log
 
+## 1.0.1
+
+### Changes
+
+* Fixed team colors
+
+### To do
+
+* Fix hyperlinks
+
 ## 1.0.0
+
 * Initial Release :D
--- src/xyz/joshrpg/SpeakNarrator/Main.java
+++ src/xyz/joshrpg/SpeakNarrator/Main.java
@@ -1,5 +1,6 @@
 package xyz.joshrpg.SpeakNarrator;
 
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -8,6 +9,7 @@ import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scoreboard.Team;
 
 import net.md_5.bungee.api.ChatMessageType;
 import net.md_5.bungee.api.chat.BaseComponent;
@@ -23,7 +25,7 @@ public class Main extends JavaPlugin {
 	
 	@Override public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (command.getName().equalsIgnoreCase("version")) {
-            sender.sendMessage("You are running version 1.0.0 of SpeakNarrator by Joshrpg");
+            sender.sendMessage("You are running version 1.0.1 of SpeakNarrator by Joshrpg");
             return true;
         }
         return false;
@@ -37,8 +39,12 @@ public class Main extends JavaPlugin {
 				return;
 			
 			BaseComponent message;
-			
-			message = new TranslatableComponent("chat.type.text", event.getPlayer().getDisplayName(), event.getMessage());
+			if (event.getPlayer().getScoreboard().getEntryTeam(event.getPlayer().getDisplayName()) != null) {
+				ChatColor color = event.getPlayer().getScoreboard().getEntryTeam(event.getPlayer().getDisplayName()).getColor();
+				message = new TranslatableComponent("chat.type.text", color + event.getPlayer().getDisplayName() + ChatColor.RESET, event.getMessage());
+			}
+			else
+				message = new TranslatableComponent("chat.type.text", event.getPlayer().getDisplayName(), event.getMessage());
 			
 			for (Player p : event.getRecipients()) {
 				p.spigot().sendMessage(ChatMessageType.CHAT, message);
@@ -47,5 +53,4 @@ public class Main extends JavaPlugin {
 			event.getRecipients().clear();
 		}
 	}
-	
 }
